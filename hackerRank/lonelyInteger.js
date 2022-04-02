c099e80670a3218f8ed49a29bda2bb22dd03d2e4
Quado2 <engchikwas404@gmail.com>
2022-04-02 22:00:54 +0100

    did counting sort

--- hackerRank/lonelyInteger.js
+++ hackerRank/lonelyInteger.js
@@ -3,7 +3,15 @@ const array = [2, 1, 2, 5, 5];
 const result = [
   ...array.reduce((r, n) => r.set(n, (r.get(n) || 0) + 1), new Map()),
 ];
-console.log({ result });
 const finalResult = result.reduce((r, v) => r[1] > v[1] ? v : r)[0]; // get the the item that appear less times
 
-console.log(finalResult);
+
+function arrayFromArgs(args) {
+  console.log(arguments)
+}
+var fruits = arrayFromArgs('Apple', 'Orange', 'Banana');
+console.log(fruits);
+
+const arr = [1,2,3];
+arr[0]++
+console.log(arr)
