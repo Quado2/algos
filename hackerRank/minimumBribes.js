1149841318cbbdf999ad30aa0d83af60f3940ebe
mac <rowlandjefffry@gmail.com>
2022-04-09 02:12:32 +0100

    finished

--- hackerRank/minimumBribes.js
+++ hackerRank/minimumBribes.js
@@ -2,42 +2,29 @@ function minimumBribes(q) {
   // Write your code here
   const length = q.length;
   let numberOfBribes = 0;
-  let lastBribed = 0;
 
-  for (let i = 0; i < length - 1; i++) {
+  for (let i = 0; i < length; i++) {
     
-    //check for when the bribe is chaotic
-    if (q[i + 1] - q[i] > 3 && lastBribed < 1) {
+    const indexDifference = q[i] - (i+1);
+
+    if(indexDifference > 2){
       numberOfBribes = "Too Chaotic";
       break;
     }
 
-    //check for edge case
-    if (q[i + 1] - q[i] < 0 && lastBribed === 0) {
-      numberOfBribes += q[i] - q[i + 1];
-      lastBribed += q[i] - q[i + 1];
-      console.log("Edge case", q[i+1], q[i], {lastBribed, numberOfBribes})
-    }
+    //Check the number of bribes each has taken.
+    //this is obtained by checking the number of people ahead of each whose number is larger 
+    //than it.
+    for(let j= q[i]-2; j<i; j++){
+      if(q[j] > q[i]){
+        numberOfBribes++
+      }
 
-    //check for actual bribe
-    if ((q[i + 1] + lastBribed) - q[i] >= 2 && lastBribed === 0) {
-      numberOfBribes += q[i + 1] - q[i] - 1;
-      lastBribed = q[i + 1] - q[i] - 1;
-      console.log("there is bribe: ", q[i + 1], q[i], q[i + 1] - q[i]);
-    } else if (q[i + 1] - q[i] >= 2 && lastBribed > 0) {
-      lastBribed -= q[i + 1] - q[i] - 1;
     }
-
-    //check for consecutive bribe
-    if((q[i + 1] + lastBribed) - q[i] >= 2 && lastBribed > 0){
-      //do here
-      console.log("Consecutive bribe: ",q[i+1], q[i],{lastBribed, numberOfBribes})
-      numberOfBribes += q[i+1] -  q[i];
-      lastBribed += q[i+1] -  q[i];
-    }
+    
   }
-
   return numberOfBribes;
 }
 
-console.log(minimumBribes([1, 2, 4, 5, 3, 6, 7, 9, 8]));
+//console.log(minimumBribes([2, 3, 4, 6, 1, 5, 9, 8, 7]));
+console.log(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]));
