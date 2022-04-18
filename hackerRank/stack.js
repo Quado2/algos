853ab669acd23717b3466b8c14c41598aef8124b
Quado2 <engchikwas404@gmail.com>
2022-04-18 23:56:05 +0100

    want to push

---
+++ hackerRank/stack.js
@@ -0,0 +1,31 @@
+function processData(input) {
+  //Enter your code here
+  const newInput = input.split("\n");
+  for(let i=1;i<newInput.length; i++){
+      switch(newInput[i][0]){
+          case 1: {
+              
+              
+              break;
+          }
+          
+          case 2: {
+              
+              
+              break;
+          }
+          
+          case 3: {
+              
+              
+              break;
+          }
+      }
+     
+    
+  }
+}
+
+const arr = [1,3,5,6]
+arr.pop();
+console.log(arr);
