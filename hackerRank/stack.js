f5ab4401a4a00c37321ede7b768aed32b1d81b57
Quado2 <engchikwas404@gmail.com>
2022-04-20 21:54:17 +0100

    continued

--- hackerRank/stack.js
+++ hackerRank/stack.js
@@ -1,31 +1,32 @@
 function processData(input) {
   //Enter your code here
+  const queue = [];
   const newInput = input.split("\n");
-  for(let i=1;i<newInput.length; i++){
-      switch(newInput[i][0]){
-          case 1: {
-              
-              
-              break;
-          }
-          
-          case 2: {
-              
-              
-              break;
-          }
-          
-          case 3: {
-              
-              
-              break;
-          }
+  const items = newInput.map((item) => {
+    return item.split(" ");
+  });
+  for (let i = 1; i < items.length; i++) {
+    switch (items[i][0]) {
+      case "1": {
+        queue.push(items[i][1]);
+        break;
       }
-     
-    
+
+      case "2": {
+        queue.splice(0, 1);
+        break;
+      }
+
+      case "3": {
+        console.log(queue[0]);
+        break;
+      }
+    }
   }
+
+  return queue;
 }
 
-const arr = [1,3,5,6]
-arr.pop();
+const arr = [1, 3, 5, 6];
+arr.splice(0, 1);
 console.log(arr);
