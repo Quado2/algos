97c140d7f359a2c66d3981192f6346a952a8cca2
Quado2 <engchikwas404@gmail.com>
2022-04-24 23:50:57 +0100

    used bind

--- linkedin.js
+++ linkedin.js
@@ -6,3 +6,11 @@ myArray.myMethod = function (l){
 setTimeout(function(){
   myArray.myMethod(1);
 }, 1000, 2);
+
+/// USIng Bind
+const theArray = ["zero","one","two"];
+const myBoundMethod = (function(i){
+  console.log(arguments.length > 0? this[l]: this)
+}).bind(theArray);
+
+myBoundMethod();
