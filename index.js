cbbae79ddc5833ac57796dae90728dd1b9994fdc
Quado2 <engchikwas404@gmail.com>
2022-04-25 22:14:50 +0100

    did shovel change

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const x = 6%2;
-const y = x? "ONe":"two";
-console.log(y)
+const someFunction1 = () => {
+    console.log(this);
+  };
+  
+  function someFunction2() {
+    console.log(this);
+  }
+  
+  const obj = {
+    someFunction1: someFunction1,
+    someFunction2: someFunction2
+  };
+  
+  obj.someFunction1();
+  obj.someFunction2();
---
+++ shovelChange.js
@@ -0,0 +1,106 @@
+function getAllIndexes(val, arr) {
+  var indexes = [],
+    i = -1;
+  while ((i = arr.indexOf(val, i + 1)) != -1) {
+    indexes.push(i);
+  }
+  return indexes;
+}
+
+var shovelChange = function (bills) {
+  // implement function here
+  let wallet = [];
+  for (let i = 0; i < bills.length; i++) {
+    switch (bills[i]) {
+      case 5: {
+        wallet.push(5);
+        break;
+      }
+
+      case 10: {
+        let all5 = getAllIndexes(5, wallet);
+        if (all5.length === 0) {
+          return false;
+        }
+        wallet.splice(all5[0], 1);
+        wallet.push(10);
+
+        break;
+      }
+
+      case 20: {
+        let all5 = getAllIndexes(5, wallet);
+        let all10 = getAllIndexes(10, wallet);
+        if (all5.length === 0 || all10.length === 0) {
+          return false;
+        }
+        wallet.splice(all5[0], 1);
+        wallet.splice(all10[0], 1);
+        wallet.push(20);
+        break;
+      }
+
+      case 50: {
+        let all5 = getAllIndexes(5, wallet);
+        let all10 = getAllIndexes(10, wallet);
+        let all20 = getAllIndexes(20, wallet);
+        if (all5.length === 0) {
+          return false;
+        }
+        if (all20.length >= 2) {
+          wallet.splice(all20[0], 1);
+          wallet.splice(all20[1], 0);
+          wallet.splice(all5[0]);
+          wallet.push(50);
+        } else if (all20.length == 1 && all10.length >= 2) {
+          wallet.splice(all20[0], 1);
+          wallet.splice(all10[0], 1);
+          wallet.splice(all10[1], 1);
+          wallet.splice(all5[0], 1);
+          wallet.push(50);
+        } else if (all10.length >= 4) {
+          for (let i = 0; i < 4; i++) {
+            wallet.splice(all10[i], 1);
+          }
+          wallet.splice(all5[0], 1);
+          wallet.push(50);
+        } else if (all10.length === 3 && all5.length >= 3) {
+          for (let i = 0; i < 3; i++) {
+            wallet.splice(all10[i], 1);
+            wallet.splice(all5[i], 1);
+          }
+          wallet.push(50);
+        } else if (all10.length === 2 && all5.length >= 5) {
+          for (let i = 0; i < 2; i++) {
+            wallet.splice(all10[i], 1);
+          }
+          for (let i = 0; i < 5; i++) {
+            wallet.splice(all5[i], 1);
+          }
+          wallet.push(50);
+        } else if (all10.length === 1 && all5.length >= 7) {
+          for (let i = 0; i < 7; i++) {
+            wallet.splice(all5[i], 1);
+          }
+          wallet.splice(all10[0], 1);
+          wallet.push(50);
+        } else if (all5.lenght >= 9) {
+          for (let i = 0; i < 9; i++) {
+            wallet.splice(all5[i], 1);
+          }
+          wallet.push(50);
+        } else {
+          return false;
+        }
+
+        break;
+      }
+    }
+  }
+
+  return true;
+};
+
+let bills = [5, 10, 5, 5, 10, 20,5,5,5,10,20,50,20];
+
+console.log(shovelChange(bills));
