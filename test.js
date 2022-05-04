d766c6a142276322f65f414c22f7d53da3624f36
Quado2 <engchikwas404@gmail.com>
2022-05-04 10:37:52 +0100

    binary trees

--- test.js
+++ test.js
@@ -1,16 +1,1 @@
-const someFunction1 = () => {
-      console.log(this);
-    };
-    
-    function someFunction2() {
-      console.log(this);
-    }
-    
-    const obj = {
-      someFunction1: someFunction1,
-      someFunction2: someFunction2
-    };
-    
-    obj.someFunction1();
-    obj.someFunction2();
-    console.log(this)
+
