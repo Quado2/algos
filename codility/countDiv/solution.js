3dd1ff45029e82b5fe35624a17e020cfbbecccc5
Quado2 <engchikwas404@gmail.com>
2022-05-06 22:54:06 +0100

    great works

---
+++ codility/countDiv/solution.js
@@ -0,0 +1,15 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A, B, K) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let divisibles = 0, interval = B-A;
+
+    let all = Math.floor(B/K);
+    let lower = Math.floor((A-1)/K);
+    return all - lower
+
+}
